333066e886f4b64c00dfbbc64beed59f99838902
Ashutosh Kumar Singh <ashusingh15673@gmail.com>
2019-08-16 13:06:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const appKey = "a1f7ea8448334671b40c520de62e11b1";
+const appKey =`a1f7ea8448334671b40c520de62e11b1`;
 
 let searchButton = document.getElementById("search-btn");
 let searchInput = document.getElementById("search-txt");
 let cityName = document.getElementById("city-name");
-let des = document.getElementById("des");
+let des=document.getElementById("des");
 let minTemp;
 let maxTemp;
 let temp;
@@ -32,16 +32,19 @@ function findWeatherDetails() {
 function theResponse(response) {
     let jsonObject = JSON.parse(response);
     console.log(jsonObject)
-    cityName.innerHTML = jsonObject.name + "," + jsonObject.sys.country;
-    des.innerHTML = jsonObject.weather[0].main;
+    cityName.innerHTML = jsonObject.name+","+jsonObject.sys.country;
+    des.innerHTML=jsonObject.weather[0].main;
     temperature.innerHTML = "Tempertaure: " + parseInt(jsonObject.main.temp - 273) + "°" + "C";
     humidity.innerHTML = "Humidity: " + jsonObject.main.humidity + "%";
     icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
-    temp = parseInt(jsonObject.main.temp - 273);
+    temp= parseInt(jsonObject.main.temp - 273);
+    
+    changeContainerBgColor(temp,minTemp,maxTemp)
 
-    changeContainerBgColor(temp, minTemp, maxTemp)
-    minTemp = parseInt(jsonObject.main.temp_min - 273);
-    maxTemp = parseInt(jsonObject.main.temp_max - 273);
+
+    minTemp= parseInt(jsonObject.main.temp_min - 273);
+    maxTemp= parseInt(jsonObject.main.temp_max - 273);
+    console.log(minTemp,maxTemp,temp)
 }
 
 function httpRequestAsync(url, callback) {
@@ -54,25 +57,29 @@ function httpRequestAsync(url, callback) {
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
-
-function changeContainerBgColor(temp, minTemp, maxTemp) {
-    var color = 'rgba(244,244,244,0.4)'
-    let color0 = col(temp)
-    let color1 = col(minTemp - 2)
-    let color2 = col(maxTemp + 2)
-
-    function col(temp) {
-        if (temp > -10 && temp <= 0) {
-            color = 'rgba(25,97,214, 0.4)'
-        } else if (temp > 1 && temp <= 15) {
-            color = 'rgba(244,244,244, 0.4)'
-        } else if (temp > 16 && temp <= 25) {
-            color = 'rgba(244,204,0, 0.4)'
-        } else if (temp > 26) {
-            color = 'rgba(216,150,0, 0.4)'
-        };
-        return color;
+function changeContainerBgColor(temp,minTemp,maxTemp){
+    var color='rgba(244,244,244,0.4)'
+   let color0=col(temp)
+   let color1=col(minTemp-2)
+   let color2=col(maxTemp+2)
+   
+   
+   
+   
+    function col(temp){
+    if(temp > -10 && temp <= 0){
+        color='rgba(25,97,214, 0.4)'
+      }else if(temp > 1 && temp <= 15){
+        color='rgba(244,244,244, 0.4)'
+      }else if(temp > 16 && temp <= 25){
+        color='rgba(244,204,0, 0.4)'
+      }else if(temp > 26){
+        color='rgba(216,150,0, 0.4)'
+      };
+    return color;
     }
-    document.body.style.backgroundImage = "-webkit-linear-gradient(" + color0 + " , " + color1 + "," + color2 + ")";
-    document.body.style.backgroundSize = 'cover'
-}
+
+    console.log(color0,color1,color2)
+    document.body.style.backgroundImage= "-webkit-linear-gradient("+ color0 +" , "+ color1 +","+color2+")";
+    document.body.style.backgroundSize='cover'
+  }
